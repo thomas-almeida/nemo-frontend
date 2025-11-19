c1f49f8448dcb2f60acf0651bc2c89211153199a
thomas almeida <contato.thomasalmeidard@gmail.com>
2025-11-19 19:18:46 -0300

    width

--- client/app/dashboard/projects/[projectId]/page.tsx
+++ client/app/dashboard/projects/[projectId]/page.tsx
@@ -239,7 +239,7 @@ export default function ProjectPage() {
     }
 
     return (
-        <div className="container mx-auto p-6 space-y-6">
+        <div className="container mx-auto p-6 space-y-6 w-[80%]">
             <div className="flex justify-between items-center">
                 <div>
                     <h1 className="text-3xl font-bold">{project.info.name}</h1>
